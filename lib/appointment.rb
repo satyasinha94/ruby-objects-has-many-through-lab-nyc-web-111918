8dc537c30b90a737e61044a87c98c6e16f59fa3a
satyasinha94 <satyasinha94@gmail.com>
2018-11-26 13:04:42 -0500

    Done

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_accessor :doctor, :date, :patient
+  @@all = []
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_reader :name, :genre
+  attr_accessor :artist
+  @@all = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+end
